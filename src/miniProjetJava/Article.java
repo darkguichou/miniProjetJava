2a4df9607b23a01d0d9742c2acc4702f0b9115e9
darkguichou <guillaume@localhost.localdomain>
2016-09-19 12:14:47 +0200

    classe Article

---
+++ src/miniProjetJava/Article.java
@@ -0,0 +1,16 @@
+package miniProjetJava;
+
+public class Article {
+
+	
+	protected String reference;
+	protected String marque;
+	protected String intitule;
+	protected float prixLocJour;
+	protected int nbStock;
+	
+	
+	
+	
+	
+}
