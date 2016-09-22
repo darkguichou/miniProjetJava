99a6677e70dd761424b6cdb56c2b743a8201a861
Lechelon Guillaume <lechelon.guillaume@gmail.com>
2016-09-22 09:04:33 +0200

    Ajout des classes Location et Client

---
+++ src/miniProjetJava/Client.java
@@ -0,0 +1,20 @@
+package miniProjetJava;
+
+public class Client {
+	
+	
+	private String nom;
+	private String prenom;
+	private String adresse;
+	
+	
+	public Client(){
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		
+		
+	}
+
+}
