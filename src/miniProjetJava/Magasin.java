defb2ae726a2b6ad11db73ecf400d0c020de1b9e
Lechelon Guillaume <lechelon.guillaume@gmail.com>
2016-09-22 09:12:32 +0200

    Ajout de la classe Magasin

---
+++ src/miniProjetJava/Magasin.java
@@ -0,0 +1,47 @@
+package miniProjetJava;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Magasin {
+	
+	
+	
+	private ArrayList<Article> articles;
+	private ArrayList<Client> clients;
+	private ArrayList<Location> locations;
+
+	
+	public void afficherArticleDispo(){
+		
+		
+		
+		
+	}
+	
+	
+	public void enregistrerLoc(){
+		
+		
+		
+		
+	}
+	
+	
+	public void afficherLocEnCours(){
+		
+		
+		
+		
+	}
+	
+	public void calculerTotal(Date dateDebut, Date dateFin){
+		
+		
+		
+		
+		
+	}
+	
+	
+}
